5a4ee6d838db3b5219e809a4815c4d738e73172e
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2020-12-04 19:21:05 +0300

    Color_enesKzlcn.js added

---
+++ anim/Color_enesKzlcn.js
@@ -0,0 +1,17 @@
+class Color_enesKzlcn extends Animator  {
+    constructor() {
+        super();
+        this.author = 'Small Simple'
+        this.desc = 'Simple digital clock'
+        let x0 = "translate(0,0)", x1 = "translate(400px,0)",x2="translate(400px,400px)",x3="translate(0,400px)"
+        let c = ["red", "black", "white", "blue","green"]
+        this.anim = { 
+                      transform:[x0,x1,x2,x3,x0],
+                      background:c
+                    }
+        this.elt = document.createElement('div')
+        this.elt.style = "width:100px;height:100px;"
+        this.time = 1000
+    }
+    update() { } 
+}
